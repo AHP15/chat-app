4f89de2d909d128881b31b64d97222005cb45403
abdessittir harkaty <abdessattarharkaty@gmail.com>
2022-02-23 13:13:07 +0100

    change urls

--- client/src/url.js
+++ client/src/url.js
@@ -1,5 +1,5 @@
 
-export const CHAT_SERVER_URL = "http://localhost:4000/";
-export const API_URL = "http://localhost:8080/";
-//https://kalam-app.herokuapp.com/
-//https://kalam-socket.herokuapp.com/
+export const CHAT_SERVER_URL = "https://kalam-socket.herokuapp.com/";
+export const API_URL = "https://kalam-app.herokuapp.com/";
+//
+//
